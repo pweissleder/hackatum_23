1d707ad80a72c98cd5576512986a0decfc97f922
Benedict Bode <benedict.bode11@gmail.com>
2023-11-17 22:30:02 +0100

    add project

---
+++ TailoredTaste/TailoredTaste/TailoredTasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TailoredTasteApp.swift
+//  TailoredTaste
+//
+//  Created by Benedict Bode Privat on 17.11.23.
+//
+
+import SwiftUI
+
+@main
+struct TailoredTasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

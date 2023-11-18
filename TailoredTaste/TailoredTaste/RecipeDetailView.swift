9a85a8f0410102d745e59d32b9cc950a286009f2
Benedict Bode <benedict.bode11@gmail.com>
2023-11-19 00:17:56 +0100

    cooked

--- TailoredTaste/TailoredTaste/RecipeDetailView.swift
+++ TailoredTaste/TailoredTaste/RecipeDetailView.swift
@@ -12,114 +12,51 @@ struct RecipeDetailView: View {
     
     let recipe: Recipe
     
+    @ObservedObject var dataViewModel = DataViewModel.shared
+    
     var body: some View {
         
         //Image und Label
         VStack(spacing: 0) {
-            VStack(alignment: .trailing){
-                Spacer()
-                HStack{
-                    LabelView(image:"dollarsign.circle.fill")
-                    LabelView(image: "leaf")
-                }
-            }.background(
-                RecipeImage(recipe: recipe, width: nil, height: nil)
-            )
-            
             ScrollView {
                 VStack() {
-                    //Image header
+                    RecipeImage(recipe: recipe, width: nil, height: nil)
                     
-                    VStack(alignment: .leading) {
-                        // Title and quik info Calories, Minm persons
-                        Text(recipe.title)
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .font(.system(.title, design: .default))
-                            .foregroundColor(Color.black)
-                            .padding(5)
-                        HStack(alignment: .center){
-                            VStack{
-                                Image(systemName: "waveform.path.ecg")
-                                    .foregroundColor(.accentColor)
-                                    .frame(width: 80, height: 80)
-                                Text("\(String(recipe.calories)) Calories")
-                                    .foregroundColor(.secondary)
-                            }
-                            
-                            VStack{
-                                Image(systemName: "person.2")
-                                    .foregroundColor(.accentColor)
-                                    .frame(width: 80, height: 80)
-                                Text("\(String(recipe.servings)) Minutes")
-                                    .foregroundColor(.secondary)
-                                
-                            }
-                            VStack{
-                                Image(systemName: "person.2")
-                                    .foregroundColor(.accentColor)
-                                    .frame(width: 80, height: 80)
-                                    .padding(5)
-                                Text("\(String(recipe.servings)) People")
-                                    .foregroundColor(.secondary)
-                            }
-                            
-                        }
-                        
-                        
-                        
-                        // Nutrition
-                        VStack(alignment: .leading, spacing: 10){
-                            HStack {
-                                Spacer()
-                                Text("Nutrition")
-                                    .font(.system(.title, design:
-                                            .default))
-                                Spacer()
+                    Button {
+                        dataViewModel.markAsCooked(id: recipe.id)
+                    } label: {
+                        Image(systemName: "frying.pan")
+                    }
+                    
+                    if let cookedEvents = dataViewModel.cookedMeals[recipe.id], let last = cookedEvents.last {
+                        Text("last cooked at " + last.description)
+                    }
+                    
+
+                    VStack(alignment: .center) {
+
+                        LazyVGrid(columns: [.init(), .init(), .init()]) {
+                            ForEach(recipe.extendedIngredients) { ingredient in
+                                IngredientView(ingredient: ingredient)
                             }
                         }
-                        .padding(5)
                         
-                        //Ingredients
-                        VStack(alignment: .center, spacing: 10){
-                            HStack {
-                                Spacer()
-                                Text("Ingredients")
-                                    .font(.system(.title, design:
-                                            .default))
-                                Spacer()
+                        ForEach(recipe.analyzedInstructions.first?.steps ?? []) { step in
+                            VStack {
+                                Text(String(step.number))
+                                    .font(.headline)
+                                    .multilineTextAlignment(.center)
+                                    .padding(7)
+                                    .overlay(Circle().stroke(Color.primary, lineWidth: 1))
+                                Text(step.step)
                             }
                         }
-                        .padding(5)
                         
-                        
-                        VStack(alignment: .leading) {
-                            ForEach(recipe.extendedIngredients, id: \.id) { ingredient in
-                                HStack {
-                                    AsyncImage(url: URL(string: "https://spoonacular.com/cdn/ingredients_100x100/" + ingredient.image)) { image in
-                                        image
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 50, height: 50)
-                                    } placeholder: {
-                                        Image("placeholder")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 50, height: 50)
-                                    }
-                                    Text(ingredient.original)
-                                }
-                            }
-                            
-                            //Instructions
-                            
-                            Text(recipe.instructions)
-                            
-                        }
                     }
                 }
                 .ignoresSafeArea(.all)
             }
         }
+        .navigationTitle(recipe.title)
     }
 }
